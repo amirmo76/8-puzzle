350999fb9e6be5ae426bbf6b14b74cbb3a038a34
Amir Mohseni <amir.mohseni7697@gmail.com>
2019-02-28 23:24:11 +0330

    IDFS added!

--- fringe.py
+++ fringe.py
@@ -1,7 +1,10 @@
+from node import Node
+
+
 class Fringe:
     data = list()
 
-    def add(self, node):
+    def add(self, node: Node):
         raise NotImplementedError("You haven't implemented this method")
 
     def get(self):
@@ -9,7 +12,7 @@ class Fringe:
 
 
 class Stack(Fringe):
-    def add(self, node):
+    def add(self, node: Node):
         self.data.append(node)
 
     def get(self):
@@ -17,10 +20,18 @@ class Stack(Fringe):
 
 
 class Queue(Fringe):
-    def add(self, node):
+    def add(self, node: Node):
         self.data.append(node)
 
     def get(self):
         node = self.data[0]
         self.data.remove(node)
         return node
+
+
+class No_Repeat_Stack(Stack):
+    def add(self, node: Node):
+        for i in self.data:
+            if node.is_equal(i):
+                return
+        super().add(node)
--- solve.py
+++ solve.py
@@ -1,10 +1,10 @@
-from fringe import Stack, Queue, Fringe
+from fringe import Stack, Queue, No_Repeat_Stack, Fringe
 from successor import successor
 from node import Node
 import time
 
 
-def search_solution(init_node: Node, successor, fringe: Fringe):
+def search_solution(init_node: Node, successor, fringe: Fringe, depth_limit: int = 0):
     fringe.add(init_node)
     prev_depth = 0
 
@@ -15,16 +15,31 @@ def search_solution(init_node: Node, successor, fringe: Fringe):
             prev_depth = candidate_node.depth
         if candidate_node.is_goal():
             return candidate_node
+        if depth_limit and candidate_node.depth > depth_limit:
+            continue
         for el in successor(candidate_node):
             fringe.add(el)
     return False
 
 
+def iterative_depth_search_solution(init_node: Node, successor, depth_limit):
+    this_round_depth_limit = 1
+    f = No_Repeat_Stack()
+    while this_round_depth_limit <= depth_limit:
+        result = search_solution(init_node, successor,
+                                 f, this_round_depth_limit)
+        if result:
+            return result
+        this_round_depth_limit += 1
+    return False
+
+
 # ===============Testing=============
-init_node = Node([[4, 1, 2], [7, 0, 3], [8, 5, 6]], 0)
-f = Queue()
+init_node = Node([[5, 3, 0], [6, 1, 8], [2, 4, 7]], 0)
+f = No_Repeat_Stack()
 start_time = time.time()
-result = search_solution(init_node, successor, f)
+# result = search_solution(init_node, successor, f, 10)
+result = iterative_depth_search_solution(init_node, successor, 20)
 end_time = time.time()
 if not result:
     print('Did not found any solution!')
